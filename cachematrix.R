eacec208473e9f5a7293ad69e0050f16e7377bd9
kartyt <karolina.tytko@gmail.com>
2021-03-24 00:25:49 +0200

    Added bodies to two fuctions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is an assignment 2 for "R programming" course
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates an R object that stores
+## a matrix and its inversion. The function doesn't calculate the inversion itself.
+## The assumption is that the matrix supplied is always a square. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setinvert <- function(solve){
+                s <<- solve
+        } 
+        getinvert <- function() s
+        list(set = set, get = get,
+             setinvert = setinvert,
+             getinvert = getinvert)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve() function checks if the inversion of the matrix (set by
+## makeCacheMatrix() function) is already calculated and store it in cache,
+## if not, the function creates the inversion with solve() function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getinvert()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setinvert(s)
+        s
 }
